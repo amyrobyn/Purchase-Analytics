f4b47ef10a9300531f47d71616cea3e467c8f4f7
amyrobyn <amykrystosik@gmail.com>
2019-07-04 00:47:30 -0700

    write report to csv with full data and comment code
    key error for prodcuts without orders

--- src/purchase analytics_2.py
+++ src/purchase analytics_2.py
@@ -1,44 +1,50 @@
-#path
+#define global variables
 path = "C:/Users/amykr/Documents/GitHub/Purchase-Analytics"#set your directory here.
+PROD_ID=0#product id is collumn 0 in products.csv
+DEPT_ID=3#dept id isc collumn 3 in products.csv
+products = {}#create empty dictionary object for products
+dept_orders = {}#create empty dictionary object for orders
+report=	[]
+i = 0#set counter to 0
 
-PROD_ID=0
-DEPT_ID=3
-products = {}
-dept_orders = {}
-
-#import products.csv from folder
-with open(path+'/input/products.csv','r') as f:
+#read products file and create map products to department
+with open(path+'/input/products.csv','r') as f:#import products.csv from folder
 	next(f)  # skip the first line in the headers
-	for line in f:
-		words = line.strip().split(',')
-		products[words[PROD_ID]] = words[DEPT_ID]
-#	print products[1][0]
+	for line in f:#loop over rows in products
+		words = line.strip().split(',')#comma delimited file. Return string without leading and trailing characters 
+		products[words[PROD_ID]] = words[DEPT_ID]#look up product id to map to department id
 
-#import order_products.csv from folder
-with open(path+'/input/order_products.csv','r') as f:
+#read order file and create map of orders to department and count of orders and reorders
+with open(path+'/input/order_products__prior.csv','r') as f:#import order_products.csv from folder
 	next(f)   #skip the first line with the headers
-	for line in f:
-		words = line.strip().split(',')
-		if products[words[1]] in dept_orders:
-			dept_orders[products[words[1]]]["orders"] += 1;
-			dept_orders[products[words[1]]]["reordered"] += int(words[3]);
-		else:
-			dept_orders[products[words[1]]] = {
-				"orders": 1, 
-				"reordered": int(words[3])
-			}
+	for line in f:#loop over rows in orders
+		words = line.strip().split(',')#comma delimited file. Return string without leading and trailing characters
+		if products[words[0]] in dept_orders:#if at least one order of product in department ## not working
+			if products[words[1]] in dept_orders:#if already in dept orders object, modify object
+				dept_orders[products[words[1]]]["orders"] += 1;#increase value of orders by 1
+				dept_orders[products[words[1]]]["reordered"] += int(words[3]);#increase value of reorder by 1
+			else:#if not already in dept orders, set default value of order and reorders
+				dept_orders[products[words[1]]] = {
+					"orders": 1, #first order sets count to 1
+					"reordered": int(words[3])#first order sets value of reorder to value of reorder
+				}
+#create report of number of product orders, number of first orders, and proportion of two.
+for dept in dept_orders:#loop over department
+		report.append(int(dept))
+		report.append(',')
+		report.append(dept_orders[dept]["orders"]) 
+		report.append(',')
+		report.append(dept_orders[dept]["orders"] - dept_orders[dept]["reordered"])
+		report.append(',')
+		report.append(float((dept_orders[dept]["orders"] - dept_orders[dept]["reordered"]) / (dept_orders[dept]["orders"] * 1.0)),2)
+		report.append('\n')
 
-		#order_products.append((words[1],words[3]))
-#	print order_products[1][1][0]
+report=sorted(report, key=dept)
 
-for order in dept_orders:
-	print ("Dept: %s   Orders: %d  first order: %d  perc: %f" 
-		%(
-			order,
-			dept_orders[order]["orders"], 
-			dept_orders[order]["orders"] - dept_orders[order]["reordered"],
-			(dept_orders[order]["orders"] - dept_orders[order]["reordered"]) / (dept_orders[order]["orders"] * 1.0)
-
-		)
-	)
-
+#write report to csv in output folder
+f = open(path+'/output/report.csv','w')#open file
+f.write('department_id'+','+'number_of_orders'+','+'number_of_first_orders'+','+'percentage'+'\n')#add header to file
+while (i<len(report)):#loop over rows in report   
+    f.write(str(report[i]))#write lines to csv
+    i = i + 1#add to counter to length of report
+f.close()#close file
