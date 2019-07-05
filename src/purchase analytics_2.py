e94e3563ffaac881650b082c290e9d90ac5f113a
amyrobyn <amykrystosik@gmail.com>
2019-07-05 15:02:15 -0700

    change order to dept in report loop

--- src/purchase analytics_2.py
+++ src/purchase analytics_2.py
@@ -7,18 +7,19 @@
 #define global variables
 path = "C:/Users/amykr/Documents/GitHub/Purchase-Analytics"#set your directory here.
 PROD_ID=0#product id is collumn 0 in products.csv
-DEPT_ID=-1#dept id is last collumn in products.csv #number of columns varies since commas in column 2 so column 3 is not always the dept id. csv reader has escape tactics to deal with this type of problem withour writing a csv reader with escape.
+DEPT_ID= -1 #dept id is last collumn in products.csv #number of columns varies since commas in column 2 so column 3 is not always the dept id. csv reader has escape tactics to deal with this type of problem withour writing a csv reader with escape.
 products = {}#create empty dictionary object for products
 dept_orders = {}#create empty dictionary object for orders
 
 #read products file and create map products to department
-with open(path+'/input/products.csv','rU') as f:#import products.csv from folder
+#with open(path+'/insight_testsuite/tests/test_1/input/products.csv','r') as f:#import order_products.csv from folder
+with open(path+'/input/products.csv','r') as f:#import products.csv from folder
 	next(f)  # skip the first line in the headers
 	for line in f:#loop over rows in products
 		column = line.strip().split(",")#comma delimited file. Return string without leading and trailing characters 
 		products[column[PROD_ID]] = int(column[DEPT_ID])#look up product id to map to department id
-
 #read order file and create map of orders to department and count of orders and reorders
+#with open(path+'/insight_testsuite/tests/test_1/input/order_products.csv','r') as f:#import order_products.csv from folder
 with open(path+'/input/order_products__prior.csv','r') as f:#import order_products.csv from folder
 	next(f)   #skip the first line with the headers
 	for line in f:#loop over rows in orders
@@ -36,13 +37,13 @@ with open(path+'/input/order_products__prior.csv','r') as f:#import order_produc
 #create report of number of product orders, number of first orders, and proportion of two.
 with open(path+'/output/report.csv','w') as f:#import order_products.csv from folder
 	f.write("department_id, number_of_orders, number_of_first_orders, percentage \n")
-	for order in sorted(dept_orders):#sort ascending 
+	for dept in sorted(dept_orders):#sort ascending 
 		f.write ("%s , %d , %d , %10.2f \n" 
 			%(
-				order,
-				dept_orders[order]["orders"], 
-				dept_orders[order]["orders"] - dept_orders[order]["reordered"],
-				(dept_orders[order]["orders"] - dept_orders[order]["reordered"]) / (dept_orders[order]["orders"] * 1.0)
+				dept,
+				dept_orders[dept]["orders"], 
+				dept_orders[dept]["orders"] - dept_orders[dept]["reordered"],
+				(dept_orders[dept]["orders"] - dept_orders[dept]["reordered"]) / (dept_orders[dept]["orders"] * 1.0)
 
 			)
 		)
