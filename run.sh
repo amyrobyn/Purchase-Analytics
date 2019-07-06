9ac50711f06f8bd9622f1dfc0315c7cd25bbbaf9
amyrobyn <amykrystosik@gmail.com>
2019-07-05 17:07:36 -0700

    run.sh and source code linux compatible paths
    linux compatible file paths to test http://ec2-3-209-212-171.compute-1.amazonaws.com/test-my-repo-link

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-#python ./src/purchase_analytics.py ./input/order_products.csv ./input/products.csv ./output/report.csv
-purchase_analytics.py 
+python ./src/purchase_analytics.py ./input/order_products.csv ./input/products.csv ./output/report.csv
+#src/purchase_analytics.py 
--- src/purchase_analytics.py
+++ src/purchase_analytics.py
@@ -5,7 +5,6 @@
 #data dictionary: https://gist.github.com/jeremystan/c3b39d947d9b88b3ccff3147dbcf6c6b
 
 #define global variables
-#path = "C:/Users/amykr/Documents/GitHub/Purchase-Analytics"#set your directory here.
 PROD_ID=0#product id is collumn 0 in products.csv
 DEPT_ID= -1 #dept id is last collumn in products.csv #number of columns varies since commas in column 2 so column 3 is not always the dept id. csv reader has escape tactics to deal with this type of problem withour writing a csv reader with escape.
 products = {}#create empty dictionary object for products
@@ -13,7 +12,7 @@ dept_orders = {}#create empty dictionary object for orders
 
 #read products file and create map products to department
 #with open(path+'/insight_testsuite/tests/test_2/input/products.csv','r') as f:#test with full data order_products.csv
-with open('../input/products.csv','r') as f:#import order_products.csv from folder
+with open('./input/products.csv','r') as f:#import order_products.csv from folder
 	next(f)  # skip the first line in the headers
 	for line in f:#loop over rows in products
 		column = line.strip().split(",")#comma delimited file. Return string without leading and trailing characters 
@@ -21,7 +20,7 @@ with open('../input/products.csv','r') as f:#import order_products.csv from fold
 
 #read order file and create map of orders to department and count of orders and reorders
 #with open(path+'/insight_testsuite/tests/test_2/input/order_products__prior.csv','r') as f:#test with full data order_products.csv
-with open('../input/order_products.csv','r') as f:#import order_products.csv from folder
+with open('./input/order_products.csv','r') as f:#import order_products.csv from folder
 	next(f)   #skip the first line with the headers
 	for line in f:#loop over rows in orders
 		column = line.strip().split(',')#comma delimited file. Return string without leading and trailing characters
@@ -36,7 +35,7 @@ with open('../input/order_products.csv','r') as f:#import order_products.csv fro
 
 #create report of number of product orders, number of first orders, and proportion of two.
 #with open(path+'/insight_testsuite/tests/test_2/output/report.csv','w') as f:#test report with full databases.
-with open('../output/report.csv','w') as f:#import order_products.csv from folder
+with open('./output/report.csv','w') as f:#import order_products.csv from folder
 	f.write("department_id, number_of_orders, number_of_first_orders, percentage \n")#add header to csv file.
 	for dept in sorted(dept_orders):#sort ascending 
 		f.write ("%s , %d , %d , %10.2f \n" 
